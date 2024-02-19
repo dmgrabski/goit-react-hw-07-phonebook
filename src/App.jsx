5bc29b05dfe6023035b88ebec808f337a220229c
dmgrabski <dmgrabski97@gmail.com>
2024-02-19 22:31:29 +0100

    update6

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Section from "./components/Section/Section";
 import ContactForm from "./components/ContactForm/ContactForm";
 import Contacts from "./components/ContactList/ContactList";
