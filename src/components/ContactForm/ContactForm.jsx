1ee69cde134c8991d1d30b1646dd1277d5630757
dmgrabski <dmgrabski97@gmail.com>
2024-02-19 22:28:46 +0100

    update5

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { nanoid } from "nanoid";
 import { useDispatch, useSelector } from "react-redux";
 import { addContact } from "../../redux/contactsSlice";
