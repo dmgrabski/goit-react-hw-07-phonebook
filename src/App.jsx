126888d1055e0abda6734fd8b4bcab4b68a607a7
dmgrabski <dmgrabski97@gmail.com>
2024-02-20 21:11:56 +0100

    up6

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux"; 
+import { fetchTasks } from "../src/redux/contactsSlice"; 
+
 import Section from "./components/Section/Section";
 import ContactForm from "./components/ContactForm/ContactForm";
 import Contacts from "./components/ContactList/ContactList";
@@ -5,6 +9,12 @@ import Filter from "./components/Filter/Filter";
 import "./App.css";
 
 const App = () => {
+  const dispatch = useDispatch(); 
+
+  useEffect(() => {
+    dispatch(fetchTasks());
+  }, [dispatch]); 
+
   return (
     <>
       <Section title="Phonebook">
